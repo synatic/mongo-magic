3a6cdbc4fb2d9ed8ea1ef4d29772486881908261
tumbonea <9964968+tumbone@users.noreply.github.com>
2026-05-27 23:40:09 +0200

    ci: Added mocharc.js configuration file

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    extension: ['js'],
+    package: './package.json',
+    reporter: 'spec',
+    timeout: 2000,
+    recursive: true,
+    globals: ['allowedDriverRequire']
+};
